083df29140c38e72c304ac4485bf51c2fe762d72
nachele <59255243+nachele@users.noreply.github.com>
2025-04-25 14:06:43 +0200

    Add files via upload

--- SpritePractice/Main.java
+++ SpritePractice/Main.java
@@ -1,10 +1,10 @@
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+
 
 class Ventana {
     int width;
@@ -79,7 +79,6 @@ class ImagenSprite extends JPanel {
                 }
                 if (keyCode == KeyEvent.VK_D) {
                     movingRight = false;  // Detener movimiento a la derecha
-
                 }
             }
             
@@ -128,22 +127,128 @@ class ImagenSprite extends JPanel {
                 }
     }
 }
+class Menu{
+    JLabel StartGame;
+    boolean run;
+    public Menu(int x,int y,int width,int height,Ventana ventana,boolean run){
+        this.StartGame = new JLabel("Start");
+        StartGame.setBounds(x, y, width, height);
+        ventana.getVentana().add(StartGame);
+        this.run = run;
+    }
+    public void clickMenu(){
+        this.StartGame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e){
+                int boton = e.getButton();
+                if(boton == MouseEvent.BUTTON1){
+                    System.out.println("Botón izquierdo presionado");
+                    run = true;
+                }else if (boton == MouseEvent.BUTTON2){
+                    System.out.println("Botón central presionado");
+                }else if(boton == MouseEvent.BUTTON3){
+                    System.out.println("Boton derecho presionado");
+                }
+            }
+        });
+    }
+
+    
+    
+}
+//conseguido array array con filas y columnas tanto como el texto ahora
+class Campo {
+    String campoTexto = "nnnnnnnnnn\n" + 
+                        "ssssssssss\n" ;
+    int fila;
+    int columna;
+    char array[][];
+    int index = 0;
+    int numeroImagenes;
+    JLabel imagenes[];
+    int altoCelda = 100;
+    int anchoCelda = 100;
+
+    public Campo(){
+        this.fila = (int)this.campoTexto.lines().count();
+        this.campoTexto.lines().forEach(linea -> {
+           
+            // Contamos los caracteres de cada línea usando el método 'length()'
+            this.columna = linea.length();
+            if( this.array == null){
+                this.array = new char[fila][columna];
+
+            }
+            for(int i = 0; i < linea.length(); i++){
+                this.array[this.index][i] = linea.charAt(i);
+            }
+            this.index+= 1;
+            // Imprimimos la línea y el número de caracteres
+        });
+
+    }
+    public void dibujarCampo(JFrame ventana){
+        this.numeroImagenes = fila * columna;
+        this.imagenes = new JLabel[this.numeroImagenes];
+    
+       // this.imagenes = new JLabel[this.numeroImagenes];
+       
+        int i = 0;
+            for(int a = 0; a < this.fila; a++){
+                for(int b = 0; b < this.columna; b++){
+                    this.imagenes[i] = new JLabel("HOLA");
+                    this.imagenes[i].setText(Character.toString(this.array[a][b]));
+                    this.imagenes[i].setBounds(b * this.anchoCelda,a * this.altoCelda, 100,100);
+                    ventana.add(this.imagenes[i]);
+                    i++;
+                }
+            }
+        
+       
+       
+    }
+
+   
+}
 
 public class Main {
+    boolean posicionrepintanda = false;
     public static void main(String[] args) {
-        Ventana ventanaObjeto = new Ventana(1920, 1080, "Sprite");
+        Main MainApp = new Main();
+        boolean run = false;
+        int anchoVentana = 1000;
+        int altoVentana = 800;
+        Ventana ventanaObjeto = new Ventana(anchoVentana, altoVentana, "Sprite");
         JFrame ventana = ventanaObjeto.getVentana();
+        Campo campo = new Campo();
+        campo.dibujarCampo(ventana);
+    
+        Menu menu = new Menu(500,300 , 100, 100, ventanaObjeto, run);
+
 
         // Creamos el objeto ImagenSprite con velocidad horizontal (speedx) y sin movimiento vertical (speedy)
-        ImagenSprite imagen = new ImagenSprite(500, 200, 5, 5); // Empezamos con velocidad 0 en X (sin movimiento)
-        ventana.add(imagen); // Añadimos el panel con el sprite a la ventana
+        ImagenSprite imagen = new ImagenSprite(-200, 200, 5, 5); // Empezamos con velocidad 0 en X (sin movimiento)
+        ventana.add(imagen); // Añadimos el panel con el sprite a la ventana;
         ventana.setVisible(true);
-
-        // Configuramos un Timer para mover la imagen cada 20ms (50 FPS)
+        imagen.setVisible(true);
+        menu.clickMenu();
+        // Configuramos un Timer para mover la imagen cada 20ms (50 FPS).*
         Timer timer = new Timer(20, e -> {
-            imagen.moverSprite();  // Mueve el sprite
-            imagen.repaint(); 
-            imagen.cambiarImagen();     // Redibuja la imagen en la nueva posición
+            if(menu.run){
+                imagen.moverSprite();
+                if(MainApp.posicionrepintanda == false){
+                    imagen.x = 400;
+                    imagen.y = 400;
+                    MainApp.posicionrepintanda = true;
+                    ventana.remove(menu.StartGame);
+                }
+                
+            }
+            imagen.cambiarImagen();
+            ventana.repaint();  
+            // Redibuja la imagen en la nueva posición
+           
+            
         });
         timer.start(); // Inicia el timer
     }
